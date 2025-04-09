4d2bd8835edd37373e2cd7b9ba39b5692f15987b
kajalchatha <kkc926@utexas.edu>
2025-04-09 10:58:29 -0500

    Add files via upload
    volume of a sphere

---
+++ volume_of_sphere.py
@@ -0,0 +1,11 @@
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+... 
+... def calculate_volume_of_sphere(radius):
+...     volume = (4/3) * math.pi * radius**3
+...     return volume
+... 
+... # Compute and print the volume for radius 30 and 40
+... print("Volume of sphere with radius 30:", calculate_volume_of_sphere(30))
+... print("Volume of sphere with radius 40:", calculate_volume_of_sphere(40))
